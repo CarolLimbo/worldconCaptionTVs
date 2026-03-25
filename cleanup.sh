faf28ed1ad59d9a1cc946c852a5c530c3491f7f9
CarolLimbo <66493158+CarolLimbo@users.noreply.github.com>
2026-03-24 21:55:46 -0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,8 @@
-pm disable-user com.netflix.ninja
-pm disable-user com.google.android.youtube.tvmusic
-pm disable-user com.google.android.youtube.tv
-pm disable-user com.amazon.amazonvideo.livingroom
-pm disable-user com.google.android.play.games
-pm disable-user com.google.android.videos
-pm disable-user com.google.android.tvrecommendations
-pm disable-user com.android.vending
-
-
-
-
-
-
+pm disable-user --user 0 com.netflix.ninja
+pm disable-user --user 0 com.google.android.youtube.tvmusic
+pm disable-user --user 0 com.google.android.youtube.tv
+pm disable-user --user 0 com.amazon.amazonvideo.livingroom
+pm disable-user --user 0 com.google.android.play.games
+pm disable-user --user 0 com.droidlogic.updater
+pm disable-user --user 0 com.google.android.tvrecommendations
+pm disable-user --user 0 com.android.vending
